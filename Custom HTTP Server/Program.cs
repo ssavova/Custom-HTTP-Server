a08660de17b291a3a3fb282159948dada30e7ad2
ssavova <stefan.a.pashov@gmail.com>
2020-01-22 21:20:22 +0200

    Make TCP listener to work async

--- Custom HTTP Server/Program.cs
+++ Custom HTTP Server/Program.cs
@@ -10,53 +10,61 @@ namespace Custom_HTTP_Server
 {
     class Program
     {
-        
-
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            const string newLine = "\r\n";
-            
+
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 8888);
             tcpListener.Start();
 
             while (true)
             {
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                Task.Run(() => ProcessClientAsync(tcpClient));
+            }
+        }
 
-                using (NetworkStream networkStream = tcpClient.GetStream())
-                {
-                    //TODO: use buffer
-                    byte[] requestBytes = new byte[10000000];
-                    int readedBytes = networkStream.Read(requestBytes, 0, requestBytes.Length);
-                    string request = Encoding.UTF8.GetString(requestBytes, 0, readedBytes);
+        private static async Task ProcessClientAsync(TcpClient client)
+        {
+            const string newLine = "\r\n";
 
-                    string responseText = @"<form action='/Account/Login' method='post'> 
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                //TODO: use buffer
+                byte[] requestBytes = new byte[10000000];
+                int readedBytes = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                string request =  Encoding.UTF8.GetString(requestBytes, 0, readedBytes);
+
+                string responseText = @"<form action='/Account/Login' method='post'> 
                                                     <input type=text name='firstname' />
                                                     <input type=text name='lastname'/>
                                                     <input type=date name='date'/>
                                                     <input type=submit value='Login'/>
                                            </form > ";
 
-                    string response = "HTTP/1.0 200 OK" + newLine
-                        + "Server: StefiServer/1.0" + newLine
-                        + "Content-Type: text/html" + newLine
-                        + "Content-Length: " + responseText.Length + newLine + newLine
-                        + responseText;
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                    networkStream.Write(responseBytes, 0, responseBytes.Length);
+                string response = "HTTP/1.0 200 OK" + newLine
+                    + "Server: StefiServer/1.0" + newLine
+                    + "Content-Type: text/html" + newLine
+                    + "Content-Length: " + responseText.Length + newLine + newLine
+                    + responseText;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
 
-                }
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
             }
         }
-
         public static async Task MockBrowser()
         {
             // this is demo how to make browser
 
             HttpClient client = new HttpClient();
-            HttpResponseMessage response =await client.GetAsync("https://softuni.bg/");
+            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
             string result = await response.Content.ReadAsStringAsync();
             File.WriteAllText("index.html", result);
         }
+
     }
 }
+    
+
+    
